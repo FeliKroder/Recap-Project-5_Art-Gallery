d0798b58b1270d7d1c84e8108c0fea69353ca50b
Roland Hesse <rolandhesse11783@gmail.com>
2023-09-13 13:45:37 +0200

    add minor changes

--- components/FavoriteButton/index.js
+++ components/FavoriteButton/index.js
@@ -1,8 +1,12 @@
 import Image from "next/image";
 // import FavIcon from "@/assets/heart.svg";
 
-export default function FavoriteButton({ artPiecesInfo, onToggleFavorite }) {
-  const { isFavorite, slug } = artPiecesInfo;
+export default function FavoriteButton({
+  artPiecesInfo,
+  onToggleFavorite,
+  slug,
+}) {
+  const { isFavorite } = artPiecesInfo;
   return (
     <button onClick={() => onToggleFavorite(slug)}>
       {isFavorite ? "Remove from favorites" : "Add to favorites"}
